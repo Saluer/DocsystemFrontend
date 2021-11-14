c5d40e3f6560977ce93adb80e80c48afbc1ba25a
Alexey <>
2021-11-14 19:19:08 +0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { WorkTableComponent } from './work-table/work-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    WorkTableComponent
   ],
   imports: [
     BrowserModule
